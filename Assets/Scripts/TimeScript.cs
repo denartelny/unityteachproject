2a3f46839842842efd58d3e52691da60b5dffcaf
Evgeny Artelny <eartelny@gmail.com>
2020-12-14 20:21:43 +0300

    dont know state

---
+++ Assets/Scripts/TimeScript.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TimeScript : MonoBehaviour
+{
+    //Солнце
+    public Transform Sun;
+    //Скорость вращения солнца
+    public float SpeedOfSun;
+    void Update()
+    {
+        //Затемнение и вращение солнца
+        Sun.Rotate(new Vector3(SpeedOfSun, 0, 0));
+    }
+}
